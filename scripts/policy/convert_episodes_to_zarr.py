e759a478e3cce405cbff7801ea28ab89c7a4bd30
Chahyon Ku <chahyon.ku@gmail.com>
2023-12-19 02:11:30 +0000

    anneal actions

--- scripts/policy/convert_episodes_to_zarr.py
+++ scripts/policy/convert_episodes_to_zarr.py
@@ -13,7 +13,8 @@ from PIL import Image
 from diffusion_policy.common.replay_buffer import ReplayBuffer
 
 def convert_dataset_to_zarr(data_root, mode, task, variation, views):
-    output_data_dir = os.path.join(data_root, mode, task, variation, 'data.zarr')
+    # output_data_dir = os.path.join(data_root, mode, task, variation, 'data.zarr')
+    output_data_dir = os.path.join(data_root, mode, task, variation, 'data.zarr2')
     shutil.rmtree(output_data_dir, ignore_errors=True)
     replay_buffer = ReplayBuffer.create_from_path(output_data_dir, mode = 'a')
 
@@ -28,12 +29,21 @@ def convert_dataset_to_zarr(data_root, mode, task, variation, views):
             low_dim_obs = pickle.load(f)
         
         for i_obs, obs in enumerate(low_dim_obs._observations):
-            next_obs = low_dim_obs._observations[i_obs+1] if i_obs < len(low_dim_obs._observations)-1 else obs
+            next_i_obs = i_obs
+            action_dist = 0
+            state = np.concatenate([obs.gripper_pose, [obs.gripper_open]])
+            while next_i_obs < len(low_dim_obs._observations) - 1 and action_dist < 0.01:
+                next_i_obs += 1
+                next_obs = low_dim_obs._observations[next_i_obs]
+                action = np.concatenate([next_obs.gripper_pose, [next_obs.gripper_open]])
+                action_dist = np.linalg.norm(action - state)
+                # break
+            # print(next_i_obs, action_dist)
             data = {
                 # 'state': np.float32(obs.task_low_dim_state),
                 # 'action': np.float32(obs.get_low_dim_data())
-                'state': np.concatenate([obs.gripper_pose, [obs.gripper_open]]),
-                'action': np.concatenate([next_obs.gripper_pose, [next_obs.gripper_open]])
+                'state': state,
+                'action': action,
             }
             data.update({
                 view: np.array(Image.open(os.path.join(episode_dir, view, f'{i_obs}.png')))
@@ -50,5 +60,5 @@ def convert_dataset_to_zarr(data_root, mode, task, variation, views):
 
 if __name__ == '__main__':
     convert_dataset_to_zarr('data/rlbench_128', 'train', 'close_jar', 'variation00', ['front_rgb', 'wrist_rgb'])
-    convert_dataset_to_zarr('data/rlbench_256', 'train', 'close_jar', 'variation00', ['front_rgb', 'wrist_rgb'])
+    # convert_dataset_to_zarr('data/rlbench_256', 'train', 'close_jar', 'variation00', ['front_rgb', 'wrist_rgb'])
     
