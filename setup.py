b331a821f176104d1276bf1cce07111c3ab92ad3
rpm <chahyon.ku@gmail.com>
2023-12-20 17:02:28 -0600

    pippable

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="slot_diffusion_policy",
-    packages=['slot_diffusion_policy'],
+    packages=['slot_diffusion_policy',
+              'slot_diffusion_policy.model'
+              'slot_diffusion_policy.model.slot',
+              'slot_diffusion_policy.dataset',],
 )
